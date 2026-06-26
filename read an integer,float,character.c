463cc3ae619e3d89ac538732ef617021c41c3750
vaishnavimudgal801-svg <vaishnavimudgal801@gmail.com>
2026-06-26 09:08:39 +0530

    Add files via upload

---
+++ read an integer,float,character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    float salary;
+    char grade;
+    printf("enter an integer,a float,and a character:");
+    scanf("%d %f %c",&age,&salary,&grade);
+    return 0;
+}
