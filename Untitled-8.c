6c13ab0d3113e789778b919e9cf85bf7d0415f58
vaishnavimudgal801-svg <vaishnavimudgal801@gmail.com>
2026-06-26 09:32:18 +0530

    Add files via upload

---
+++ Untitled-8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int divident=21534178903,product,rem;
+    for(int temp=divident;temp!=0;temp=temp/10)
+    {
+        rem=temp%10;
+        if(rem==2||rem==3||rem==5||rem==7)
+    }
+{
+product=product*rem;
+printf("product");
+}
+return 0;
+}
