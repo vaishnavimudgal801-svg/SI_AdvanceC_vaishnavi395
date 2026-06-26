c00718ccd7cbd3bd72f57cd9dce97688b12a65b9
vaishnavimudgal801-svg <vaishnavimudgal801@gmail.com>
2026-06-26 09:29:24 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int sq,temp;
+    for(int i=0;i>=0;i++)
+    sq=i*i;
+temp=sq;
+whiletemp>0
+{
+    rem=temp%10;
+    sum=sum+rem;
+    temp=temp/10;
+}
+if(sum==i)
+{
+    countN++;
+
+}
+}
+if(count N==15)
+{
+    printf("15th neon number is %d",i);
+    break;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
