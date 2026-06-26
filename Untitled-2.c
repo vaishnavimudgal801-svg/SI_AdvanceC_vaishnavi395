c8f49fe82339f600cf3cbadcebb2bb20c3d44f50
vaishnavimudgal801-svg <vaishnavimudgal801@gmail.com>
2026-06-26 09:30:05 +0530

    Add files via upload

---
+++ Untitled-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int rem,sum=0,square,temp1;
+    for(int i=1;i<1000;i++)
+}
+square=i*i;
+sum=0;
+for(int temp1=square;temp1>0;temp1=temp1/10)
+{
+    rem=temp1%10;
+    sum=sum+rem;
+}
+if(sum==i)
+{
+    printf("%d is a neon number\n",i);
+}
+return 0;
+}
+
+
+
+
