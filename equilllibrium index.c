49c635f254691c2a01ede9fcb9e4a5a1313d0ff9
vaishnavimudgal801-svg <vaishnavimudgal801@gmail.com>
2026-06-26 09:07:02 +0530

    Add files via upload

---
+++ equilllibrium index.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int arr[5]={3,5,8,5,3};
+int size=5 ;
+int i,j;
+int lsum,rsum;
+for(i=0;i<size;i++)
+{
+lsum=0;
+rsum=0;
+for(j=i-1;j>=0;j++)
+lsum+=arr[j];
+}
+for(k=i+1;k<size-1;k++)
+{
+    rsum+=arr[k];
+
+if(lsum==rsum)
+{
+    equi=[i]
+    count++;
+    break;
+    printf("%d",a[i]);
+}
+return 0;
+}
